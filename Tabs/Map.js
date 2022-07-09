ed0fccaf832d9feae7c3e5a9003092f0b5a78064
MohammedHossane <mohammedhossaneo0.0@gmail.com>
2022-07-09 14:50:41 -0700

    added geolocation

--- Tabs/Map.js
+++ Tabs/Map.js
@@ -1,21 +1,53 @@
-import * as React from 'react';
+import React, { useState, useEffect } from 'react';
 import MapView from 'react-native-maps';
 import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import * as Location from 'expo-location';
+
 
 export default function Map() {
+
+  const [location, setLocation] = useState(null);
+  const [mapRegion, setMapRegion] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+
+      let mapRegion = {
+        latitude: location.coords.latitude,
+        longitude: location.coords.longitude,
+        latitudeDelta: .01,
+        longitudeDelta: .01,
+        zoom: 5, }
+
+        setMapRegion(mapRegion)
+      
+    })();
+  }, []);
+
+  let text = 'Waiting..';
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    text = JSON.stringify(location);
+  }
+
   return (
     <View style={styles.container}>
-    <MapView
+      <Text style={styles.paragraph}>{text}</Text>
+      <MapView
       style={styles.map}
-      initialRegion={{
-        latitude: 34.0172521,
-        longitude: -118.2782022,
-        latitudeDelta: 0.0922,
-        longitudeDelta: 0.0421,
-        zoom: 30,
-      }}
+   
+      initialRegion={mapRegion}
     />
-  </View>
+    </View>
 );
 }
 
