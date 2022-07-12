138f80b693e70a22c72a1d917e88b36c73bcd038
MohammedHossane <mohammedhossaneo0.0@gmail.com>
2022-07-12 14:07:47 -0700

    actionbar

--- App.js
+++ App.js
@@ -26,7 +26,12 @@ export default function App() {
           (<Image source={require('./assets/Variant3.jpg')} style={{ width: 32, height: 32 }}/>)}} />
       </Tabs.Navigator>
     </NavigationContainer>
-    </NativeBaseProvider>
+    </NativeBaseProvider>,
+     <NativeBaseProvider>
+     <Center flex={1} px="3">
+         <Example />
+     </Center>
+   </NativeBaseProvider>
   );
 }
 
@@ -53,3 +58,29 @@ const styles = StyleSheet.create({
 });
 
 // https://maps.googleapis.com/maps/api/staticmap?key=AIzaSyBSS0O17zdL_oCDLTKS67aDph6BWwCxgV4&center=47.66670827132002,-122.3844921033355&zoom=13&format=png&maptype=roadmap&style=feature:administrative.land_parcel%7Celement:labels%7Cvisibility:off&style=feature:poi%7Celement:labels.text%7Cvisibility:off&style=feature:poi.business%7Cvisibility:off&style=feature:road%7Celement:labels.icon%7Cvisibility:off&style=feature:road.local%7Celement:labels%7Cvisibility:off&style=feature:transit%7Cvisibility:off&size=480x360
+
+import { Button, Actionsheet, useDisclose, Center } from 'native-base';
+
+function Example() {
+  const {
+    isOpen,
+    onOpen,
+    onClose
+  } = useDisclose();
+  return <>
+
+      <Actionsheet isOpen={true} onClose={onClose}>
+        <Actionsheet.Content>
+          <Box w="100%" h={60} px={4} justifyContent="center">
+            <Text fontSize="16" color="gray.500" _dark={{
+            color: 'gray.300'
+          }}>
+              
+            </Text>
+          </Box>
+          <Actionsheet.Item>Map</Actionsheet.Item>
+          <Actionsheet.Item>Share</Actionsheet.Item>
+        </Actionsheet.Content>
+      </Actionsheet>
+    </>;
+}
