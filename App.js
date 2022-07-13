dde5721eb859c3557a1812c365487bcd3d13ef97
TXT-70 <txt-53@TXT-70.local>
2022-07-13 11:53:04 -0700

    added new bottom drawer

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 //the navigation stuff that is annoying to deal w/
 import { NavigationContainer } from "@react-navigation/native";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 // Import our custom components
 import Scan from "./Tabs/Scan";
@@ -12,22 +12,53 @@ import { NativeBaseProvider, Box } from "native-base";
 import { StyleSheet, Text, View, Image } from "react-native";
 
 // Create an instance of Bottom Tab navigator
-const Tabs = createBottomTabNavigator();
+const Stack = createNativeStackNavigator();
 
 //display my satufu
 export default function App() {
   return (
     <NativeBaseProvider>
-    <NavigationContainer>
-    <Tabs.Navigator initialRouteName="Map">
-    <Tabs.Screen  name = "Map" component={Map} options={{tabBarIcon: () => 
-          (<Image source={require('./assets/Variant3.jpg')} style={{ width: 32, height: 32 }}/>)}} />
-        <Tabs.Screen name="Scan" component={Scan} options={{tabBarIcon: () => 
-          (<Image source={require('./assets/Group42.jpg')} style={{ width: 32, height: 32 }}/>)}} />
-    <Tabs.Screen name="Report" component={Report} options={{tabBarIcon: () => 
-          (<Image source={require('./assets/Group42.jpg')} style={{ width: 32, height: 32 }}/>)}} />
-      </Tabs.Navigator>
-    </NavigationContainer>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Map" >
+          <Stack.Screen
+            name="Map"
+            component={Map}
+            options={{
+              headerShown: false,
+              tabBarIcon: () => (
+                <Image
+                  source={require("./assets/Variant3.jpg")}
+                  style={{ width: 32, height: 32 }}
+                />
+              ),
+            }}
+          />
+          {/* <Stack.Screen
+            name="Scan"
+            component={Scan}
+            options={{
+              tabBarIcon: () => (
+                <Image
+                  source={require("./assets/Group42.jpg")}
+                  style={{ width: 32, height: 32 }}
+                />
+              ),
+            }}
+          /> */}
+          {/* <Stack.Screen
+            name="Report"
+            component={Report}
+            options={{
+              tabBarIcon: () => (
+                <Image
+                  source={require("./assets/Group42.jpg")}
+                  style={{ width: 32, height: 32 }}
+                />
+              ),
+            }}
+          /> */}
+        </Stack.Navigator>
+      </NavigationContainer>
     </NativeBaseProvider>
   );
 }
@@ -42,12 +73,6 @@ const styles = StyleSheet.create({
   },
 });
 
-
-
-
-
-
-
 // bruh
 
 // https://maps.googleapis.com/maps/api/staticmap?key=AIzaSyBSS0O17zdL_oCDLTKS67aDph6BWwCxgV4&center=47.66670827132002,-122.3844921033355&zoom=13&format=png&maptype=roadmap&style=feature:administrative.land_parcel%7Celement:labels%7Cvisibility:off&style=feature:poi%7Celement:labels.text%7Cvisibility:off&style=feature:poi.business%7Cvisibility:off&style=feature:road%7Celement:labels.icon%7Cvisibility:off&style=feature:road.local%7Celement:labels%7Cvisibility:off&style=feature:transit%7Cvisibility:off&size=480x360
@@ -55,25 +80,24 @@ const styles = StyleSheet.create({
 // import { Button, Actionsheet, useDisclose, Center } from 'native-base';
 
 function Example() {
-  const {
-    isOpen,
-    onOpen,
-    onClose
-  } = useDisclose();
-  return <>
-
+  const { isOpen, onOpen, onClose } = useDisclose();
+  return (
+    <>
       <Actionsheet isOpen={true} onClose={onClose} disableOverlay>
         <Actionsheet.Content>
           <Box w="100%" h={40} px={4} justifyContent="center">
-            <Text fontSize="16" color="gray.500" _dark={{
-            color: 'gray.300'
-          }}>
-              
-            </Text>
+            <Text
+              fontSize="16"
+              color="gray.500"
+              _dark={{
+                color: "gray.300",
+              }}
+            ></Text>
           </Box>
           <Actionsheet.Item>Scan</Actionsheet.Item>
           <Actionsheet.Item>Report</Actionsheet.Item>
         </Actionsheet.Content>
       </Actionsheet>
-    </>;
- }
+    </>
+  );
+}
--- Tabs/Map.js
+++ Tabs/Map.js
@@ -1,19 +1,24 @@
-import React, { useState, useEffect } from 'react';
-import MapView, { Marker } from 'react-native-maps';
-import { StyleSheet, Text, View, Dimensions } from 'react-native';
-import * as Location from 'expo-location';
-
+import React, { useState, useEffect } from "react";
+import MapView, { Marker } from "react-native-maps";
+import { StyleSheet, Text, View, Dimensions } from "react-native";
+import * as Location from "expo-location";
+import Animated from "react-native-reanimated";
+import BottomSheet from "reanimated-bottom-sheet";
 
 export default function Map() {
-
   const [location, setLocation] = useState(null);
   const [mapRegion, setMapRegion] = useState(null);
   const [errorMsg, setErrorMsg] = useState(null);
+  const [isMapLoading, setIsMapLoading] = useState(true);
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false); // drawer is closed when app loads
+
+  const sheetRef = React.useRef(null);
+
   useEffect(() => {
     (async () => {
       let { status } = await Location.requestForegroundPermissionsAsync();
-      if (status !== 'granted') {
-        setErrorMsg('Permission to access location was denied');
+      if (status !== "granted") {
+        setErrorMsg("Permission to access location was denied");
         return;
       }
 
@@ -23,374 +28,449 @@ export default function Map() {
       let mapRegion = {
         latitude: location.coords.latitude,
         longitude: location.coords.longitude,
-        latitudeDelta: .01,
-        longitudeDelta: .01,
+        latitudeDelta: 0.01,
+        longitudeDelta: 0.01,
         zoom: 5,
-      }
+      };
 
-      setMapRegion(mapRegion)
-
+      setMapRegion(mapRegion);
+      setIsMapLoading(false);
     })();
   }, []);
 
-  let text = 'Waiting..';
-  if (errorMsg) {
-    text = errorMsg;
-  } else if (location) {
-    text = JSON.stringify(location);
-  }
+  // let text = "Waiting..";
+  // if (errorMsg) {
+  //   text = errorMsg;
+  // } else if (location) {
+  //   text = JSON.stringify(location);
+  // }
+
+  const onClose = () => {
+    setIsDrawerOpen(false);
+  };
+
+  const renderContent = () => {
+    return (
+      <View style={{ backgroundColor: "red" }}>
+        <Text>Get directions to your location</Text>
+      </View>
+    );
+  };
+
+  const renderHeader = () => {
+    return (
+      <View style={{ backgroundColor: "blue" }}>
+        <Text>header</Text>
+      </View>
+    );
+  };
+
+  const onExpand = () => {
+    console.log("opening drawer");
+  };
+
+  const onCollapse = () => {
+    console.log("closing drawer");
+  };
 
   return (
     <View style={styles.container}>
-      <Text style={styles.paragraph}>{text}</Text>
-      {mapRegion === null ? null : <MapView
-       style1={styles.mapStyle}
-       initialRegion1={{
-       latitude: 34.017349,
-       longitude: -118.278343,
-       latitudeDelta: 0.0922,
-       longitudeDelta: 0.0421,
-        }}
-        style={styles.map}
-        showsUserLocation={true}
-        initialRegion={mapRegion}
-        customMapStyle={mapStyle}
-      >
-        {/* <Marker coordinate = {{latitude: 34.039660,longitude: -118.378700}}
+      {isMapLoading === true ? (
+        <Text style={styles.paragraph}>Waiting</Text>
+      ) : null}
+
+      {errorMsg ? <Text style={styles.paragraph}>{errorMsg}</Text> : null}
+
+      {mapRegion === null ? null : (
+        <MapView
+          style1={styles.mapStyle}
+          initialRegion1={{
+            latitude: 34.017349,
+            longitude: -118.278343,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+          style={styles.map}
+          showsUserLocation={true}
+          initialRegion={mapRegion}
+          customMapStyle={mapStyle}
+        >
+          {/* <Marker coordinate = {{latitude: 34.039660,longitude: -118.378700}}
          pinColor = {"red"} // any color
          title={"Angel's House"}
          description={"This is where Angel lives."}/>   */}
 
-         <Marker coordinate = {{latitude: 34.033080,longitude: -118.292020}}
-         pinColor = {"lightblue"} // any color
-         title={"76 Gas Station"}
-         description={"Pump #2, beware"}/>  
+          <Marker
+            coordinate={{ latitude: 34.03308, longitude: -118.29202 }}
+            pinColor={"lightblue"} // any color
+            title={"76 Gas Station"}
+            description={"Pump #2, beware"}
+          />
 
-         <Marker coordinate = {{latitude: 34.035740,longitude: -118.291230}}
-         pinColor = {"lightblue"} // any color
-         title={"Cheveron Gas Station"}
-         description={"Pump #7, beware"}/>  
+          <Marker
+            coordinate={{ latitude: 34.03574, longitude: -118.29123 }}
+            pinColor={"lightblue"} // any color
+            title={"Cheveron Gas Station"}
+            description={"Pump #7, beware"}
+          />
 
-         <Marker coordinate = {{latitude: 34.017810,longitude: -118.292020}}
-         pinColor = {"lightblue"} // any color
-         title={"ARCO Gas Station"}
-         description={"Pump #5, beware"}/>  
+          <Marker
+            coordinate={{ latitude: 34.01781, longitude: -118.29202 }}
+            pinColor={"lightblue"} // any color
+            title={"ARCO Gas Station"}
+            description={"Pump #5, beware"}
+          />
+        </MapView>
+      )}
 
-      </MapView>}
+      <BottomSheet
+        enabledBottomClamp={true}
+        ref={sheetRef}
+        snapPoints={[350, 300, 0]}
+        borderRadius={10}
+        renderContent={renderContent}
+        renderHeader={renderHeader}
+      />
+
+      {/* <BottomDrawer
+        onCollapse={onCollapse}
+        onExpand={onExpand}
+        containerHeight={400}
+        offset={0}
+        startUp={false}
+      >
+        {renderContent()}
+      </BottomDrawer> */}
+
+      {/* <Actionsheet hideDragIndicator={false} isOpen={isDrawerOpen} onClose={onClose} disableOverlay>
+        <Actionsheet.Content>
+          <Box w="100%" h={40} px={4} justifyContent="center">
+            <Text
+              fontSize="16"
+              color="gray.500"
+              _dark={{
+                color: "gray.300",
+              }}
+            ></Text>
+          </Box>
+          <Actionsheet.Item>Scan</Actionsheet.Item>
+          <Actionsheet.Item>Report</Actionsheet.Item>
+        </Actionsheet.Content>
+      </Actionsheet> */}
     </View>
   );
 }
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
   map: {
-    width: Dimensions.get('window').width,
-    height: Dimensions.get('window').height,
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("window").height,
   },
 });
 
 const mapStyle = [
   {
-    "featureType": "administrative.country",
-    "elementType": "labels.icon",
-    "stylers": [
+    featureType: "administrative.country",
+    elementType: "labels.icon",
+    stylers: [
       {
-        "visibility": "on"
-      }
-    ]
+        visibility: "on",
+      },
+    ],
   },
   {
-    "featureType": "administrative.land_parcel",
-    "elementType": "labels.icon",
-    "stylers": [
+    featureType: "administrative.land_parcel",
+    elementType: "labels.icon",
+    stylers: [
       {
-        "visibility": "off"
-      }
-    ]
+        visibility: "off",
+      },
+    ],
   },
   {
-    "featureType": "administrative.locality",
-    "elementType": "labels.icon",
-    "stylers": [
+    featureType: "administrative.locality",
+    elementType: "labels.icon",
+    stylers: [
       {
-        "visibility": "off"
-      }
-    ]
+        visibility: "off",
+      },
+    ],
   },
   {
-    "featureType": "administrative.neighborhood",
-    "elementType": "labels.icon",
-    "stylers": [
+    featureType: "administrative.neighborhood",
+    elementType: "labels.icon",
+    stylers: [
       {
-        "visibility": "off"
-      }
-    ]
+        visibility: "off",
+      },
+    ],
   },
   {
-    "featureType": "administrative.province",
-    "elementType": "labels.icon",
-    "stylers": [
+    featureType: "administrative.province",
+    elementType: "labels.icon",
+    stylers: [
       {
-        "visibility": "off"
-      }
-    ]
+        visibility: "off",
+      },
+    ],
   },
   {
-    "featureType": "landscape.man_made",
-    "elementType": "labels.icon",
-    "stylers": [
+    featureType: "landscape.man_made",
+    elementType: "labels.icon",
+    stylers: [
       {
-        "visibility": "off"
-      }
-    ]
+        visibility: "off",
+      },
+    ],
   },
   {
-    "featureType": "landscape.natural",
-    "elementType": "labels.icon",
-    "stylers": [
+    featureType: "landscape.natural",
+    elementType: "labels.icon",
+    stylers: [
       {
-        "visibility": "off"
-      }
-    ]
+        visibility: "off",
+      },
+    ],
   },
   {
-    "featureType": "landscape.natural.landcover",
-    "elementType": "labels.icon",
-    "stylers": [
+    featureType: "landscape.natural.landcover",
+    elementType: "labels.icon",
+    stylers: [
       {
-        "visibility": "off"
-      }
-    ]
+        visibility: "off",
+      },
+    ],
   },
   {
-    "featureType": "landscape.natural.terrain",
-    "elementType": "labels.icon",
-    "stylers": [
+    featureType: "landscape.natural.terrain",
+    elementType: "labels.icon",
+    stylers: [
       {
-        "visibility": "on"
-      }
-    ]
+        visibility: "on",
+      },
+    ],
   },
   {
-    "featureType": "poi.attraction",
-    "elementType": "labels.icon",
-    "stylers": [
+    featureType: "poi.attraction",
+    elementType: "labels.icon",
+    stylers: [
       {
-        "visibility": "off"
-      }
-    ]
+        visibility: "off",
+      },
+    ],
   },
   {
-    "featureType": "poi.business",
-    "stylers": [
+    featureType: "poi.business",
+    stylers: [
       {
-        "visibility": "on"
-      }
-    ]
+        visibility: "on",
+      },
+    ],
   },
   {
-    "featureType": "poi.business",
-    "elementType": "geometry.fill",
-    "stylers": [
+    featureType: "poi.business",
+    elementType: "geometry.fill",
+    stylers: [
       {
-        "visibility": "on"
-      }
-    ]
+        visibility: "on",
+      },
+    ],
   },
   {
-    "featureType": "poi.business",
-    "elementType": "geometry.stroke",
-    "stylers": [
+    featureType: "poi.business",
+    elementType: "geometry.stroke",
+    stylers: [
       {
-        "visibility": "on"
-      }
-    ]
+        visibility: "on",
+      },
+    ],
   },
   {
-    "featureType": "poi.business",
-    "elementType": "labels",
-    "stylers": [
+    featureType: "poi.business",
+    elementType: "labels",
+    stylers: [
       {
-        "visibility": "off"
-      }
-    ]
+        visibility: "off",
+      },
+    ],
   },
   {
-    "featureType": "poi.business",
-    "elementType": "labels.icon",
-    "stylers": [
+    featureType: "poi.business",
+    elementType: "labels.icon",
+    stylers: [
       {
-        "visibility": "on"
+        visibility: "on",
       },
       {
-        "weight": 1.5
-      }
-    ]
+        weight: 1.5,
+      },
+    ],
   },
   {
-    "featureType": "poi.business",
-    "elementType": "labels.text",
-    "stylers": [
+    featureType: "poi.business",
+    elementType: "labels.text",
+    stylers: [
       {
-        "visibility": "on"
-      }
-    ]
+        visibility: "on",
+      },
+    ],
   },
   {
-    "featureType": "poi.business",
-    "elementType": "labels.text.fill",
-    "stylers": [
+    featureType: "poi.business",
+    elementType: "labels.text.fill",
+    stylers: [
       {
-        "visibility": "on"
-      }
-    ]
+        visibility: "on",
+      },
+    ],
   },
   {
-    "featureType": "poi.business",
-    "elementType": "labels.text.stroke",
-    "stylers": [
+    featureType: "poi.business",
+    elementType: "labels.text.stroke",
+    stylers: [
       {
-        "visibility": "on"
-      }
-    ]
+        visibility: "on",
+      },
+    ],
   },
   {
-    "featureType": "poi.government",
-    "elementType": "labels.icon",
-    "stylers": [
+    featureType: "poi.government",
+    elementType: "labels.icon",
+    stylers: [
       {
-        "visibility": "off"
-      }
-    ]
+        visibility: "off",
+      },
+    ],
   },
   {
-    "featureType": "poi.medical",
-    "elementType": "labels.icon",
-    "stylers": [
+    featureType: "poi.medical",
+    elementType: "labels.icon",
+    stylers: [
       {
-        "visibility": "off"
-      }
-    ]
+        visibility: "off",
+      },
+    ],
   },
   {
-    "featureType": "poi.park",
-    "elementType": "labels.icon",
-    "stylers": [
+    featureType: "poi.park",
+    elementType: "labels.icon",
+    stylers: [
       {
-        "visibility": "off"
-      }
-    ]
+        visibility: "off",
+      },
+    ],
   },
   {
-    "featureType": "poi.place_of_worship",
-    "elementType": "labels.icon",
-    "stylers": [
+    featureType: "poi.place_of_worship",
+    elementType: "labels.icon",
+    stylers: [
       {
-        "visibility": "off"
-      }
-    ]
+        visibility: "off",
+      },
+    ],
   },
   {
-    "featureType": "poi.school",
-    "elementType": "labels.icon",
-    "stylers": [
+    featureType: "poi.school",
+    elementType: "labels.icon",
+    stylers: [
       {
-        "visibility": "off"
-      }
-    ]
+        visibility: "off",
+      },
+    ],
   },
   {
-    "featureType": "poi.sports_complex",
-    "elementType": "labels.icon",
-    "stylers": [
+    featureType: "poi.sports_complex",
+    elementType: "labels.icon",
+    stylers: [
       {
-        "visibility": "off"
-      }
-    ]
+        visibility: "off",
+      },
+    ],
   },
   {
-    "featureType": "road.arterial",
-    "elementType": "labels.icon",
-    "stylers": [
+    featureType: "road.arterial",
+    elementType: "labels.icon",
+    stylers: [
       {
-        "visibility": "off"
-      }
-    ]
+        visibility: "off",
+      },
+    ],
   },
   {
-    "featureType": "road.highway",
-    "elementType": "labels.icon",
-    "stylers": [
+    featureType: "road.highway",
+    elementType: "labels.icon",
+    stylers: [
       {
-        "visibility": "off"
-      }
-    ]
+        visibility: "off",
+      },
+    ],
   },
   {
-    "featureType": "road.highway.controlled_access",
-    "elementType": "labels.icon",
-    "stylers": [
+    featureType: "road.highway.controlled_access",
+    elementType: "labels.icon",
+    stylers: [
       {
-        "visibility": "on"
-      }
-    ]
+        visibility: "on",
+      },
+    ],
   },
   {
-    "featureType": "road.local",
-    "elementType": "labels.icon",
-    "stylers": [
+    featureType: "road.local",
+    elementType: "labels.icon",
+    stylers: [
       {
-        "visibility": "off"
-      }
-    ]
+        visibility: "off",
+      },
+    ],
   },
   {
-    "featureType": "transit.line",
-    "elementType": "labels.icon",
-    "stylers": [
+    featureType: "transit.line",
+    elementType: "labels.icon",
+    stylers: [
       {
-        "visibility": "off"
-      }
-    ]
+        visibility: "off",
+      },
+    ],
   },
   {
-    "featureType": "transit.station",
-    "elementType": "labels.icon",
-    "stylers": [
+    featureType: "transit.station",
+    elementType: "labels.icon",
+    stylers: [
       {
-        "visibility": "off"
-      }
-    ]
+        visibility: "off",
+      },
+    ],
   },
   {
-    "featureType": "transit.station.airport",
-    "elementType": "labels.icon",
-    "stylers": [
+    featureType: "transit.station.airport",
+    elementType: "labels.icon",
+    stylers: [
       {
-        "visibility": "off"
-      }
-    ]
+        visibility: "off",
+      },
+    ],
   },
   {
-    "featureType": "transit.station.bus",
-    "elementType": "labels.icon",
-    "stylers": [
+    featureType: "transit.station.bus",
+    elementType: "labels.icon",
+    stylers: [
       {
-        "visibility": "off"
-      }
-    ]
+        visibility: "off",
+      },
+    ],
   },
   {
-    "featureType": "transit.station.rail",
-    "elementType": "labels.icon",
-    "stylers": [
+    featureType: "transit.station.rail",
+    elementType: "labels.icon",
+    stylers: [
       {
-        "visibility": "off"
-      }
-    ]
-  }
-]
+        visibility: "off",
+      },
+    ],
+  },
+];
